0b53b9dcc5f43222f5844ccec1559327440c8c96
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-06-13 16:58:47 -0300

    Tarefa 3 concluída

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,53 @@
 /* Script Global */
-import {applyInputRangeStyle} from "./inputRange.js";
-import {albumList} from "./albumsDatabase.js";
+import { applyInputRangeStyle } from "./inputRange.js";
+import { albumList } from "./albumsDatabase.js";
 
-function routine(){
-    applyInputRangeStyle();
+function routine() {
+  applyInputRangeStyle();
+  renderCardItems(albumList);
 }
 
 routine();
 
-function createAlbumCard(albumList){
-    // criando tags
-    let card = document.createElement('li');
-    let cover = document.createElement('img');
-    let name = document.createElement('h3');
-    let cardTop = document.createElement('div');
-    let band = document.createElement('p');
-    let genre = document.createElement('p');
-    let cardBottom = document.createElement('div');
-    let price = document.createElement('p');
-    let buyButton = document.createElement('button');
+function renderCardItems(albumList) {
+  let albumCardList = document.querySelector(".album-list");
+  albumCardList.innerHTML = null;
 
-    // organizando tags
-    cardTop.append(band, genre);
-    cardBottom.append(price, buyButton);
-    card.append(cover, name, cardTop, cardBottom);
+  albumList.forEach((element) => {
+    albumCardList.append(createAlbumCard(element));
+  });
+}
 
-    // atribuindo classes
-    
+function createAlbumCard(albumListItem) {
+  // criando tags
+  let card = document.createElement("li");
+  let cover = document.createElement("img");
+  let name = document.createElement("h3");
+  let cardTop = document.createElement("div");
+  let band = document.createElement("p");
+  let genre = document.createElement("p");
+  let cardBottom = document.createElement("div");
+  let price = document.createElement("p");
+  let buyButton = document.createElement("button");
 
+  // organizando tags
+  cardTop.append(band, genre);
+  cardBottom.append(price, buyButton);
+  card.append(cover, name, cardTop, cardBottom);
 
+  // atribuindo classes
+  card.classList.add("album-list__card");
+  cardTop.classList.add("card-top");
+  cardBottom.classList.add("card-bottom");
+  buyButton.classList.add("buy-button");
+
+  // atribuindo conteúdo
+  cover.src = albumListItem.img;
+  name.innerText = albumListItem.title;
+  band.innerText = albumListItem.band;
+  genre.innerText = albumListItem.genre;
+  price.innerText = `R$ ${albumListItem.price}`;
+  buyButton.innerText = "Comprar";
+
+  return card;
 }
