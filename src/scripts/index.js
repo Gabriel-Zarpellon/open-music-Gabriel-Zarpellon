ab22e09972bfb789a98dc8de444aadae37f5c269
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-06-13 15:49:02 -0300

    Tarefa 3 em andamento

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,32 @@
 /* Script Global */
+import {applyInputRangeStyle} from "./inputRange.js";
+import {albumList} from "./albumsDatabase.js";
 
+function routine(){
+    applyInputRangeStyle();
+}
+
+routine();
+
+function createAlbumCard(albumList){
+    // criando tags
+    let card = document.createElement('li');
+    let cover = document.createElement('img');
+    let name = document.createElement('h3');
+    let cardTop = document.createElement('div');
+    let band = document.createElement('p');
+    let genre = document.createElement('p');
+    let cardBottom = document.createElement('div');
+    let price = document.createElement('p');
+    let buyButton = document.createElement('button');
+
+    // organizando tags
+    cardTop.append(band, genre);
+    cardBottom.append(price, buyButton);
+    card.append(cover, name, cardTop, cardBottom);
+
+    // atribuindo classes
+    
+
+
+}
